da5ff2a5257aa29aa341ff9ad05c63ee31a12cc1
reema-2 <reeemaa23@gmail.com>
2019-10-20 09:51:46 +0300

    updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ var rename = require("gulp-rename");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var sourcemaps = require("gulp-sourcemaps");
-var styleSass = "./scss/style.scss";
-var styleCss = "./css";
+var styleSass = "./scss-en/govsa-ds-en.scss";
+var styleSassar = "./scss-ar/govsa-ds-ar.scss";
+var styleCss = "./css/en";
+var styleCssar = "./css/ar";
 var stylewatch = './scss/**/*.scss';
 
 gulp.task("style", function() {
@@ -39,8 +41,42 @@ gulp.task("styleMin", function() {
     .pipe(gulp.dest(styleCss));
 });
 
-gulp.task('init', gulp.series(gulp.parallel('style', 'styleMin')));
+
+gulp.task("style-ar", function() {
+  gulp.src(styleSassar)
+      .pipe(
+      sass({
+        errorLogToConsole: true,
+      })
+    ).on("error", console.error.bind(console))
+    .pipe(autoprefixer({
+      browsers:['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(styleCssar));
+});
+
+gulp.task("styleMin-ar", function() {
+  gulp.src(styleSassar)
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        errorLogToConsole: true,
+        outputStyle: 'compressed'
+      })
+    ).on("error", console.error.bind(console))
+    .pipe(autoprefixer({
+      browsers:['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(styleCssar));
+});
+
+
+gulp.task('init', gulp.series(gulp.parallel('style', 'styleMin', 'style-ar', 'styleMin-ar')));
 
 gulp.task('watch', function() {
-gulp.watch(stylewatch , gulp.parallel('style', 'styleMin'));
+gulp.watch(stylewatch , gulp.parallel('style', 'styleMin', 'style-ar', 'styleMin-ar'));
 });
